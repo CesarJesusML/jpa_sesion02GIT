66c865b5bdeb048697cd146ee41ea870e06d5a82
Cesar <i202122731@cibertec.edu.pe>
2023-09-15 21:42:19 -0500

    validando usuario con procedimientos almacenados

--- src/main/java/main/Pruebas.java
+++ src/main/java/main/Pruebas.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
 
 import model.TbUsuario;
 
@@ -14,13 +15,17 @@ public class Pruebas {
 		System.out.println("===JPA CRUD====");
 		EntityManagerFactory fabrica = Persistence.createEntityManagerFactory("JPA_clase01x");
 		EntityManager em = fabrica.createEntityManager();
-		
-		List<TbUsuario> lstUsuarios = em.createQuery("Select a From TbUsuario a", TbUsuario.class).getResultList();
-		System.out.println("==============================================");
-		
-		/*Mostrar cantidad de usuarios*/
-		System.out.println("nro de usuarios:" +lstUsuarios.size());
-		System.out.println("==============================================");
+	
+		Query query = em.createNativeQuery("{call usp_validaAcceso(?, ?)}", TbUsuario.class);
+		query.setParameter(1, "U001@gmail.com");
+		query.setParameter(2, "10001");
+		TbUsuario u = (TbUsuario)query.getSingleResult();
+		if(u == null){
+			System.out.println("Usuario no existe");
+		}else{
+			System.out.println("Bienvenido: "+u.getNomUsua());
+		}
+		System.out.println("-------------------------------");
 
 	}
 
